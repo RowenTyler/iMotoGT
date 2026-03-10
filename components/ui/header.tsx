9e8fec57666b36f481a189f1b0afa91d86d4567f
RowenTyler <tyler.rowend@gmail.com>
2026-03-10 22:35:22 +0000

    header updates

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -213,8 +213,8 @@ export function Header({
           <Image
             src="/imoto-icon-new.png"
             alt="MOTO GT Logo"
-            width={200}
-            height={60}
+            width={60}
+            height={30}
             className="object-contain cursor-pointer"
             style={{ filter: "none" }}
             priority
